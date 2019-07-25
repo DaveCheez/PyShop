9f18bd43aa4cd0c58fb8a6306ef85a5d384cff0d
DaveCheez <davecheez21@gmail.com>
2019-07-25 15:40:11 +0100

    added some setup scripts

---
+++ venv.sh
@@ -0,0 +1,52 @@
+export projectslug=cloudstore
+export projectgit="${projectslug}".git
+export projectDB="${projectslug}"_db
+export USERNAME=davecheez
+export USER=davecheez21_gmail_com
+export projectDBuser="${USERNAME}"_user
+export localip="34.76.126.193"
+export GIT_DIR=/var/repo/"${projectgit}"
+export GIT_WORK_TREE=/var/www/"${projectslug}"
+export virtualenvbin="/var/www/$projectslug/bin"
+
+echo
+echo "Use default blank django project? (y/n)"
+read defaultDjangoProject
+defaultDjangoProjectResponse="$(echo -n '${defaultDjangoProject}' | sed -e 's/[^[:alnum:]]/-/g' | tr -s '_' | tr A-Z a-z)"
+echo "${defaultDjangoProject}"
+
+sudo mkdir /var/www/"$projectslug"/
+cd ~/
+ln -s /var/www/"$projectslug"/ ~/
+
+sudo mkdir /var/repo/
+sudo mkdir /var/repo/"$projectgit"/
+sudo chmod 775 --recursive /home/"$USER"/
+sudo chmod 775 --recursive /var/repo/
+sudo chmod 775 --recursive /var/www/
+
+git config --global core.autocrlf input
+
+cd /var/repo/"$projectgit"/
+git clone https://github.com/codingforentrepreneurs/CFE-Blank-Project . --bare
+git --work-tree="/var/www/$projectslug" --git-dir="/var/repo/$projectgit" checkout -f
+virtualenv -p python3.6 /var/www/"$projectslug"
+cd /var/www/"$projectslug"/src/
+sudo rm -rf /var/www/"$projectslug"/src/cfehome/
+
+"$virtualenvbin"/python3 -m pip install -r /var/www/"$projectslug"/src/requirements.txt
+
+cat <<EOT >> /var/repo/"$projectgit"/hook/post-receive
+git --work-tree=/var/www/"$projectslug" --git-dir=/var/repo/"$projectgit" checkout -f
+
+"$virtualenvbin"/python3 -m pip install -r /var/www/"$projectslug"/src/requirements.txt
+sudo rm -rf /var/www/"$projectslug"/src/cfehome/
+
+supervisorctl reread
+supervisorctl update
+EOT
+
+chmod +x /var/repo/"$projectgit"/hooks/post-receive
+
+exit 0
+
