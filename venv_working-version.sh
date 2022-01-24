ead9a64dfa69c65f65c6ca5c5a03a76275f32d38
Dave Cheez <davecheez21@gmail.com>
2022-01-24 14:27:47 +0000

    Update venv_working-version.sh

--- venv_working-version.sh
+++ venv_working-version.sh
@@ -1,49 +1,50 @@
 export projectslug=cloudstore
-export projectgit="${projectslug}".git
-export projectDB="${projectslug}"_db
+export projectgit=${projectslug}.git
+export projectDB=${projectslug}_db
 export USERNAME=davecheez
-export USER=davecheez21_gmail_com
-export projectDBuser="${USERNAME}"_user
+export projectDBuser=${USERNAME}_user
 export localip="34.76.126.193"
-export GIT_DIR=/var/repo/"${projectgit}"
-export GIT_WORK_TREE=/var/www/"${projectslug}"
-export virtualenvbin="/var/www/$projectslug/bin"
+export GIT_DIR=${HOME}/repo/${projectgit}/
+export GIT_WORK_TREE=${HOME}/src/${projectslug}/
+export VENVBIN=${HOME}/src/${projectslug}/bin
 
 echo
 echo "Use default blank django project? (y/n)"
+
 read defaultDjangoProject
 defaultDjangoProjectResponse="$(echo -n '${defaultDjangoProject}' | sed -e 's/[^[:alnum:]]/-/g' | tr -s '_' | tr A-Z a-z)"
 
-sudo mkdir /var/www/"$projectslug"/
-cd ~/
-ln -s /var/www/"$projectslug"/ ~/
+sudo mkdir ${HOME}/src
+sudo mkdir ${HOME}/repo
 
-sudo mkdir /var/repo/
-sudo mkdir /var/repo/"$projectgit"/
-sudo chmod 775 --recursive /home/"$USER"/
-sudo chmod 775 --recursive /var/repo/
-sudo chmod 775 --recursive /var/www/
+sudo mkdir $GIT_WORK_TREE
+sudo mkdir $GIT_DIR
+
+sudo chmod 775 --recursive ${HOME}
+sudo chmod 775 --recursive $GIT_DIR
+sudo chmod 775 --recursive $GIT_WORK_TREE
 
 git config --global core.autocrlf input
 
-cd /var/repo/"$projectgit"/
-git clone git@github.com:DaveCheez/DjangoBoiler.git . --bare
-git --work-tree="/var/www/$projectslug" --git-dir="/var/repo/$projectgit" checkout -f
-virtualenv -p python3.6 /var/www/"$projectslug"
-cd /var/www/"$projectslug"/
+cd $GIT_DIR
 
-"$virtualenvbin"/python3 -m pip install -r /var/www/"$projectslug"/requirements.txt
+git clone git@github.com/DaveCheez/PyShop.git . --bare
+git --work-tree=$GIT_WORK_TREE --git-dir=${GIT_DIR} checkout -f
+python -m venv $VENVBIN
+cd $GIT_WORK_TREE
 
-cat <<EOT >> /var/repo/"$projectgit"/hookS/post-receive
-git --work-tree=/var/www/"$projectslug" --git-dir=/var/repo/"$projectgit" checkout -f
+${VENVBIN}/python3 -m pip install -r /$GIT_WORK_TREE/requirements.txt
 
-"$virtualenvbin"/python3 -m pip install -r /var/www/"$projectslug"/requirements.txt
+cat <<EOT >> /var/repo/"$projectgit"/hooks/post-receive
+git --work-tree=${GIT_WORK_TREE} --git-dir=${GIT_DIR} checkout -f
+
+${VENVBIN}/python3 -m pip install -r /$GIT_WORK_TREE/requirements.txt
 
 supervisorctl reread
 supervisorctl update
 EOT
 
-chmod +x /var/repo/"$projectgit"/hooks/post-receive
+chmod +x ${GIT_DIR}/hooks/post-receive
 
 exit 0
 
